e0e33466f9922a0458c3cbdad38e77a5d8059928
fieldydnb <fieldydnb@gmail.com>
2025-11-07 19:00:27 +0100

    v3

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'STVR LiveTV',
-      theme: ThemeData.dark(),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: const Color(0xFF121212),
       debugShowCheckedModeBanner: false,
       home: const HomePage(),
     );
