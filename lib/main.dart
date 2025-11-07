30be56ff596935614f7ef50c75fcaebcac6be2be
fieldydnb <fieldydnb@gmail.com>
2025-11-07 21:09:52 +0100

    v3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:video_player/video_player.dart';
+import 'package:better_player/better_player.dart';
 
 void main() => runApp(const MyApp());
 
@@ -92,97 +92,60 @@ class PlayerPage extends StatefulWidget {
 }
 
 class _PlayerPageState extends State<PlayerPage> {
-  late final VideoPlayerController _controller;
-  bool _ready = false;
+  late BetterPlayerController _betterPlayerController;
 
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.networkUrl(Uri.parse(widget.url))
-      ..initialize().then((_) {
-        setState(() => _ready = true);
-        _controller
-          ..setLooping(true)
-          ..play();
-      });
+    _initializePlayer();
+  }
+
+  void _initializePlayer() {
+    BetterPlayerConfiguration betterPlayerConfiguration =
+        const BetterPlayerConfiguration(
+      autoPlay: true,
+      looping: true,
+      fullScreenByDefault: false,
+      allowedScreenSleep: false,
+    );
+
+    _betterPlayerController = BetterPlayerController(betterPlayerConfiguration);
+
+    BetterPlayerDataSource dataSource = BetterPlayerDataSource(
+      BetterPlayerDataSourceType.dash,
+      widget.url,
+    );
+
+    _betterPlayerController.setupDataSource(dataSource);
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _betterPlayerController.dispose();
     super.dispose();
   }
 
-  Future<void> _openFullscreen() async {
-    if (!_ready) return;
-    await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-    await SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight],
-    );
-    await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (_) => FullscreenPlayer(controller: _controller, title: widget.title)),
-    );
-    await SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-    await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          IconButton(icon: const Icon(Icons.fullscreen), onPressed: _openFullscreen),
-        ],
-      ),
-      body: Center(
-        child: _ready
-            ? Stack(
-                alignment: Alignment.bottomRight,
-                children: [
-                  AspectRatio(
-                    aspectRatio: _controller.value.aspectRatio == 0 ? 16 / 9 : _controller.value.aspectRatio,
-                    child: VideoPlayer(_controller),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(12),
-                    child: FloatingActionButton.small(
-                      onPressed: _openFullscreen,
-                      child: const Icon(Icons.fullscreen),
-                    ),
-                  ),
-                ],
-              )
-            : const CircularProgressIndicator(),
-      ),
-    );
-  }
-}
-
-class FullscreenPlayer extends StatelessWidget {
-  final VideoPlayerController controller;
-  final String title;
-  const FullscreenPlayer({super.key, required this.controller, required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: () {
-          if (controller.value.isPlaying) {
-            controller.pause();
-          } else {
-            controller.play();
-          }
-        },
-        child: Center(
-          child: AspectRatio(
-            aspectRatio: controller.value.aspectRatio == 0 ? 16 / 9 : controller.value.aspectRatio,
-            child: VideoPlayer(controller),
-          ),
+    return WillPopScope(
+      onWillPop: () async {
+        await SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+        await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: BetterPlayer(controller: _betterPlayerController),
+              ),
+            ),
+          ],
         ),
       ),
     );
