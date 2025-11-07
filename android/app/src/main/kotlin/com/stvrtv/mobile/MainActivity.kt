7741a49bf59a255ef99a070b082af2d2b4ac8315
fieldydnb <fieldydnb@gmail.com>
2025-11-07 19:50:05 +0100

    v3

--- android/app/src/main/kotlin/com/stvrtv/mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/stvrtv/mobile/MainActivity.kt
@@ -1,12 +1,6 @@
 package com.stvrtv.mobile
 
-import android.os.Bundle
-import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-    }
 }
