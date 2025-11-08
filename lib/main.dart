7442746675dc813c6fcd74332ecea98b6df701ef
fieldydnb <fieldydnb@gmail.com>
2025-11-08 01:10:18 +0100

    v1.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:video_player/video_player.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'STVR LiveTV',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color(0xFF121212),
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
-    );
-  }
-}
-
-class Channel {
-  final String name;
-  final String url;
-  final String logoAsset;
-  const Channel(this.name, this.url, this.logoAsset);
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  List<Channel> get channels => [
-    Channel('Jednotka', 'https://livesim2.dashif.org/livesim2/testpic_2s/Manifest.mpd', 'assets/logos/jednotka.png'),
-    Channel('Dvojka', 'https://livesim2.dashif.org/livesim2/WAVE/av/combined.mpd', 'assets/logos/dvojka.png'),
-    Channel(':24', 'https://5g.towercom.sk/rtvs24-dash-mp4/manifest.mpd', 'assets/logos/stvr24.png'),
-    Channel('Šport', 'https://livesim2.dashif.org/livesim2/testpic4_8s/Manifest600.mpd', 'assets/logos/sport.png'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('STVR LiveTV')),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: GridView.count(
-          crossAxisCount: 2,
-          mainAxisSpacing: 16,
-          crossAxisSpacing: 16,
-          childAspectRatio: 1,
-          children: [
-            for (final ch in channels)
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => PlayerPage(title: ch.name, url: ch.url),
-                    ),
-                  );
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(color: Colors.grey[800]!, width: 1),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        ch.logoAsset,
-                        width: 100,
-                        height: 100,
-                        fit: BoxFit.contain,
-                        errorBuilder: (context, error, stackTrace) {
-                          return Container(
-                            width: 100,
-                            height: 100,
-                            color: Colors.grey[300],
-                            child: const Icon(Icons.image_not_supported),
-                          );
-                        },
-                      ),
-                      const SizedBox(height: 12),
-                      Text(
-                        ch.name,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+import 'package:wakelock_plus/wakelock_plus.dart';
 
 class PlayerPage extends StatefulWidget {
   final String title;
@@ -122,11 +21,18 @@ class _PlayerPageState extends State<PlayerPage> {
   late VideoPlayerController _controller;
   bool _isInitialized = false;
   bool _isPlaying = false;
+  bool _isFullscreen = false;
   String? _errorMessage;
 
   @override
   void initState() {
     super.initState();
+    WakelockPlus.enable();  // Zabránime výspavaniu obrazovky
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.portraitUp,
+    ]);
     _initializePlayer();
   }
 
@@ -149,53 +55,110 @@ class _PlayerPageState extends State<PlayerPage> {
     }
   }
 
+  void _toggleFullscreen() {
+    setState(() {
+      _isFullscreen = !_isFullscreen;
+    });
+    
+    if (_isFullscreen) {
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.landscapeRight,
+        DeviceOrientation.landscapeLeft,
+      ]);
+    } else {
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+          overlays: SystemUiOverlay.values);
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+      ]);
+    }
+  }
+
   @override
   void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+    WakelockPlus.disable();
     _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: _isInitialized
-          ? GestureDetector(
-              onTap: () {
-                setState(() {
-                  _isPlaying ? _controller.pause() : _controller.play();
-                });
-              },
-              child: Center(
-                child: AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      VideoPlayer(_controller),
-                      if (!_isPlaying)
-                        Container(
-                          color: Colors.black54,
-                          child: const Icon(
-                            Icons.play_arrow,
-                            color: Colors.white,
-                            size: 64,
+    return WillPopScope(
+      onWillPop: () async {
+        if (_isFullscreen) {
+          _toggleFullscreen();
+          return false;
+        }
+        return true;
+      },
+      child: Scaffold(
+        appBar: !_isFullscreen ? AppBar(title: Text(widget.title)) : null,
+        body: _isInitialized
+            ? GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _isPlaying ? _controller.pause() : _controller.play();
+                  });
+                },
+                onDoubleTap: _toggleFullscreen,
+                child: Center(
+                  child: AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        VideoPlayer(_controller),
+                        if (!_isPlaying)
+                          Container(
+                            color: Colors.black54,
+                            child: const Icon(
+                              Icons.play_arrow,
+                              color: Colors.white,
+                              size: 64,
+                            ),
+                          ),
+                        Positioned(
+                          bottom: 10,
+                          right: 10,
+                          child: GestureDetector(
+                            onTap: _toggleFullscreen,
+                            child: Container(
+                              padding: const EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                color: Colors.black54,
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              child: Icon(
+                                _isFullscreen
+                                    ? Icons.fullscreen_exit
+                                    : Icons.fullscreen,
+                                color: Colors.white,
+                                size: 24,
+                              ),
+                            ),
                           ),
                         ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
+              )
+            : Center(
+                child: _errorMessage != null
+                    ? Text(
+                        _errorMessage!,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(color: Colors.white),
+                      )
+                    : const CircularProgressIndicator(),
               ),
-            )
-          : Center(
-              child: _errorMessage != null
-                  ? Text(
-                      _errorMessage!,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(color: Colors.white),
-                    )
-                  : const CircularProgressIndicator(),
-            ),
+      ),
     );
   }
 }
